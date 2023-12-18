d48428a6d19f09478e575028c84a41d3a0d5b94d
unknown <R.Badry@ISDT.LOCAL>
2023-12-18 15:03:18 +0200

    day 1 - part 2 & 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: '<h1>Hello World</h1>',
   styleUrls: ['./app.component.scss']
+  // styles: ['h3{color: darkblue}']
 })
 export class AppComponent {
-  title = 'CourseDemo';
+  title = 'Front-end track 2023';
 }
